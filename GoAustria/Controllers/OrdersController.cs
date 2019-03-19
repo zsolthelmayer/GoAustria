d401f2bdd9a93deca050563458318a0483508a64
zsolthelmayer <33486595+zsolthelmayer@users.noreply.github.com>
2019-03-19 20:26:59 +0100

    order modif

--- GoAustria/Controllers/OrdersController.cs
+++ GoAustria/Controllers/OrdersController.cs
@@ -56,7 +56,7 @@ namespace GoAustria.Controllers
 
 
 			new MessageHelper().SendMessage("info@ausztriamunka.hu", value.OrdEmail, "Megrendelés - (Rendelésszám: " + now.ToString("yyyyMMddHHmmss")+ ")", mailBody);
-			//new MessageHelper().SendMessage("info@ausztriamunka.hu", "info@ausztriamunka.hu", "Megrendelés - (Rendelésszám: " + now.ToString("yyyyMMddHHmmss") + ")", body);
+			new MessageHelper().SendMessage("info@ausztriamunka.hu", "info@ausztriamunka.hu", "Megrendelés - (Rendelésszám: " + now.ToString("yyyyMMddHHmmss") + ")", mailBody);
 
 		}
 
